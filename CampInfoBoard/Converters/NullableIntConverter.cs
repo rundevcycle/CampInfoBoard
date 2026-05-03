e87c6300e35c53474dc685c7da6d686e13511a0c
Andy Smith <andy@rundevcycle.com>
2026-05-03 19:22:55 -0300

    Fix validation when setting a nullable field back to empty.

---
+++ CampInfoBoard/Converters/NullableIntConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace CampInfoBoard.Converters
+{
+    public class NullableIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value?.ToString() ?? "";
+        }
+
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string? text = value as string;
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+
+            if (int.TryParse(text, out int result))
+            {
+                return result;
+            }
+
+            return WpfBinding.DoNothing;
+        }
+    }
+}
